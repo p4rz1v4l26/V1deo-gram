d5caf0a96dcf5e604c1d2d1812a04dfa090fcf40
KaifSayyad <kaifalisayyad@gmail.com>
2024-03-02 00:02:08 +0530

    aidfsdabfkdajfbdamkc

--- V1deo-gram/apps/server/src/consumer.js
+++ V1deo-gram/apps/server/src/consumer.js
@@ -1,6 +1,9 @@
 const Kafka = require("node-rdkafka");
+const fs = require("fs");
+const zlib = require("zlib");
 
 const TOPIC_NAME = "demo_topic";
+const OUTPUT_FILE = "output.mp4";
 
 const stream = new Kafka.createReadStream(
   {
@@ -15,6 +18,35 @@ const stream = new Kafka.createReadStream(
   { topics: [TOPIC_NAME] }
 );
 
+const writeStream = fs.createWriteStream(OUTPUT_FILE);
+
 stream.on("data", (message) => {
-  console.log("Got message using SSL:", message.value.toString());
+  try {
+    const compressedData = zlib.gzipSync(message.value);
+    writeStream.write(compressedData);
+    console.log("Received and wrote compressed data to file:", OUTPUT_FILE);
+  } catch (err) {
+    console.error("Error processing message:", err);
+  }
+});
+
+stream.on("end", () => {
+  writeStream.end();
+  console.log("Finished receiving and compressing video.");
+});
+
+stream.consumer.on("event.error", (err) => {
+  console.error("Consumer error:", err);
+});
+
+stream.consumer.on("disconnected", () => {
+  console.log("Consumer disconnected.");
+});
+
+stream.consumer.on("event.log", (log) => {
+  console.log("Consumer log:", log);
+});
+
+stream.consumer.on("event.stats", (stats) => {
+  console.log("Consumer stats:", stats);
 });
--- V1deo-gram/apps/server/src/producer.js
+++ V1deo-gram/apps/server/src/producer.js
@@ -1,14 +1,15 @@
 const Kafka = require("node-rdkafka");
+const fs = require("fs");
 
 const TOPIC_NAME = "demo_topic";
 
 const producer = new Kafka.Producer({
-  'metadata.broker.list': "kafka-17907454-v1deo-gram.a.aivencloud.com:26681",
+  "metadata.broker.list": "kafka-17907454-v1deo-gram.a.aivencloud.com:26681",
   "security.protocol": "ssl",
   "ssl.key.location": "service.key",
   "ssl.certificate.location": "service.cert",
   "ssl.ca.location": "ca.pem",
-  dr_cb: true,
+  dr_cb: true
 });
 
 producer.connect();
@@ -26,7 +27,8 @@ const produceMessagesOnSecondIntervals = async () => {
         continue;
       }
 
-      const message = `Hello from Node using SSL ${++i}!`;
+      const filePath = "vid.mp4"; // Replace with the actual file path
+      const fileData = fs.readFileSync(filePath);
       producer.produce(
         // Topic to send the message to
         TOPIC_NAME,
@@ -34,14 +36,14 @@ const produceMessagesOnSecondIntervals = async () => {
         // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
         null,
         // Message to send. Must be a buffer
-        Buffer.from(message),
+        fileData,
         // for keyed messages, we also specify the key - note that this field is optional
         null,
         // you can send a timestamp here. If your broker version supports it,
         // it will get added. Otherwise, we default to 0
         Date.now()
       );
-      console.log(`Message sent: ${message}`);
+      console.log(`Message sent: vid.mp4`);
     } catch (err) {
       console.error("A problem occurred when sending our message");
       console.error(err);
